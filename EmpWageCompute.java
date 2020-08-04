9e7e1646b20d127010ca22593aa9921e7336d3df
Ruchir Dixit <ruchirtd96@gmail.com>
2020-08-04 21:46:04 +0530

    Calculated Daily wage

--- EmpWageCompute.java
+++ EmpWageCompute.java
@@ -1,10 +1,11 @@
 public class EmpWageCompute{
 	public static void main(String[] arg){
 		final int IS_FULL_TIME=1;
+		final int EMP_WAGE_PR_HR=20;
+		int empHrs=0;
 		double empCheck=Math.floor(Math.random()*10)%2;
 		if (empCheck == IS_FULL_TIME)
-			System.out.println("Employee present");
-		else
-			System.out.println("Employee absent");
+			empHrs=8;
+		System.out.println("Daily Wage of emp is:"+empHrs*EMP_WAGE_PR_HR);
 	}
 }
