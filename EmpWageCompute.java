e05164182bbac130fb4989112815dab2de7a19cf
Ruchir Dixit <ruchirtd96@gmail.com>
2020-08-04 21:39:53 +0530

    Emp Attendance branch

--- EmpWageCompute.java
+++ EmpWageCompute.java
@@ -1,5 +1,10 @@
 public class EmpWageCompute{
 	public static void main(String[] arg){
-		System.out.println("Welcome to employee wage computation program on master!");
+		final int IS_FULL_TIME=1;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee present");
+		else
+			System.out.println("Employee absent");
 	}
 }
