953efe1df69bf56a91ad0875c1caae4989321c24
Ruchir Dixit <ruchirtd96@gmail.com>
2020-08-04 21:50:11 +0530

    Added part time

--- EmpWageCompute.java
+++ EmpWageCompute.java
@@ -1,11 +1,14 @@
 public class EmpWageCompute{
 	public static void main(String[] arg){
 		final int IS_FULL_TIME=1;
+		final int IS_PART_TIME=2;
 		final int EMP_WAGE_PR_HR=20;
 		int empHrs=0;
-		double empCheck=Math.floor(Math.random()*10)%2;
+		double empCheck=Math.floor(Math.random()*10)%3;
 		if (empCheck == IS_FULL_TIME)
 			empHrs=8;
+		else if (empCheck == IS_PART_TIME)
+			empHrs=4;
 		System.out.println("Daily Wage of emp is:"+empHrs*EMP_WAGE_PR_HR);
 	}
 }
