fe35bb3b3a7a1a209635bc5a6788f8cc7279e773
Ruchir Dixit <ruchirtd96@gmail.com>
2020-08-05 13:11:18 +0530

    Daily wage for 20 days

--- EmpWageCompute.java
+++ EmpWageCompute.java
@@ -1,12 +1,20 @@
-public class EmpWageCompute{
-	public static void main(String[] arg){
-		final int IS_FULL_TIME=1;
-		final int IS_PART_TIME=2;
-		final int EMP_WAGE_PR_HR=20;
-		int empHrs=0;
+public class EmpWageCompute
+{
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;
+	public static final int EMP_RATE_PR_HOUR=20;
+	public static final int EMP_DAYS_PR_MONTH=20;
+	public static void main(String[] args)
+	{
+	int empHrs=0;
+	int empWage=0;
+	int totalEmpWage=0;
+	for(int day=0;day<EMP_DAYS_PR_MONTH;day++)
+	{
 		int empCheck=(int)Math.floor(Math.random()*10)%3;
+		System.out.println("Emp Check is:"+empCheck);
 		switch(empCheck){
-			case IS_FULL_TIME:
+			case	IS_FULL_TIME:
 					empHrs=8;
 					break;
 			case IS_PART_TIME:
@@ -16,6 +24,9 @@ public class EmpWageCompute{
 					empHrs=0;
 					break;
 		}
-		System.out.println("Daily Wage of emp is:"+empHrs*EMP_WAGE_PR_HR);
+		empWage=empHrs*EMP_RATE_PR_HOUR;
+		totalEmpWage=totalEmpWage+empWage;
+	}
+		System.out.println("Total Employee wage is:"+totalEmpWage);
 	}
 }
