992b77d5a71c698b0ccbf9d558376fba0e476b2d
Ruchir Dixit <ruchirtd96@gmail.com>
2020-08-05 13:18:44 +0530

    Added While Loop

--- EmpWageCompute.java
+++ EmpWageCompute.java
@@ -3,30 +3,31 @@ public class EmpWageCompute
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
 	public static final int EMP_RATE_PR_HOUR=20;
-	public static final int EMP_DAYS_PR_MONTH=20;
+	public static final int EMP_DAYS_PR_MONTH=2;
+	public static final int MAX_HRS_PER_MONTH=10;
 	public static void main(String[] args)
 	{
-	int empHrs=0;
-	int empWage=0;
-	int totalEmpWage=0;
-	for(int day=0;day<EMP_DAYS_PR_MONTH;day++)
-	{
-		int empCheck=(int)Math.floor(Math.random()*10)%3;
-		System.out.println("Emp Check is:"+empCheck);
-		switch(empCheck){
-			case	IS_FULL_TIME:
-					empHrs=8;
-					break;
-			case IS_PART_TIME:
-					empHrs=4;
-					break;
-			default:
-					empHrs=0;
-					break;
+		int empHrs=0,empWage=0,totalEmpWage=0,totalWorkingDays=0,totalEmpHrs=0;
+		while(totalWorkingDays<EMP_DAYS_PR_MONTH && totalEmpHrs<=MAX_HRS_PER_MONTH)
+		{
+			totalWorkingDays++;
+			int empCheck=(int)Math.floor(Math.random()*10)%3;
+			switch(empCheck)
+			{
+				case	IS_FULL_TIME:
+						empHrs=8;
+						break;
+				case IS_PART_TIME:
+						empHrs=4;
+						break;
+				default:
+						empHrs=0;
+						break;
+			}
+			totalEmpHrs+=empHrs;
+			empWage=empHrs*EMP_RATE_PR_HOUR;
+			totalEmpWage=totalEmpWage+empWage;
 		}
-		empWage=empHrs*EMP_RATE_PR_HOUR;
-		totalEmpWage=totalEmpWage+empWage;
-	}
-		System.out.println("Total Employee wage is:"+totalEmpWage);
+		System.out.println("Employee wage is:"+totalEmpWage);
 	}
 }
